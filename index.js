95e790922db13f2aba3f5c40c50e07f3314e3470
lunarvibes45 <tealdeal@tutanota.com>
2022-07-11 02:28:09 -0700

    updated index.js and index.html

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-document.querySelector('button').addEventListener('click', getRequest)
+document.querySelector('.button').addEventListener('click', getRequest)
 
 async function getRequest(){
     const days = document.querySelector('input').value
     
     try{
-        const response = await fetch(`https://git.heroku.com/crystals-api.git/api/${days}`)
+        const response = await fetch(`https://crystals-api.herokuapp.com/api/${days}`)
+        
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('.day').innerHTML = data.day
-        document.querySelector('.stone').innerHTML = data.stone
-        document.querySelector('.planet'),innerHTML = data.planet
-       }
-
-    catch{
+        document.querySelector('.day').innerText = data.day
+        document.querySelector('.stone').innerText = data.stone
+        document.querySelector('img').src = data.image
+        document.querySelector('.planet').innerText = data.planet
+        
+       }catch(error){
         console.log(error)
     }
 }
